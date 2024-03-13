b7fc2092d3fec48440371742f6db0c0819053166
MKM <kaanmesum@gmail.com>
2024-03-13 16:25:37 +0300

    Profile App

--- API/Apps/Profile/models.py
+++ API/Apps/Profile/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Stats(models.Model):
     total_games = models.IntegerField()
     total_wins = models.IntegerField()
