92fdfc0b346e55401aa3b30110b7565b055964cb
Berat <100490399+bertt6@users.noreply.github.com>
2024-03-17 13:50:35 +0300

    Update settings.py

--- API/API/settings.py
+++ API/API/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '0a4fd7e3567eab20bab2f2d6682ea96d0d865050bfcde9c293aa7f9ec48bce55'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'API.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
+        "NAME": 'transcendenceDB',
+        "USER": 'postgres',
+        "PASSWORD": '123',
+        "HOST": '127.0.0.1',
+        "PORT": '5432',
     }
 }
 
