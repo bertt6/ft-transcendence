720e823aef321775258b7db9f2de1721f22bc8fa
yugurlu <yusufugurlu39@outlook.com>
2024-03-30 16:39:05 +0300

    websocket urls changed

--- API/API/asgi.py
+++ API/API/asgi.py
@@ -4,19 +4,23 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
 
+from Apps.Chat import consumers
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.settings")
 
 django_asgi_app = get_asgi_application()
 
-import Apps.Chat.routing
+websocket_urlpatterns = [
+    re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+]
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(Apps.Chat.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
 )
