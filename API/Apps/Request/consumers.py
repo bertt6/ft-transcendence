cdb14de3618cfeb21e9627b808ddc3f7b86259fb
MKM <kaanmesum@gmail.com>
2024-04-09 01:02:26 +0300

    request thingies

--- API/Apps/Request/consumers.py
+++ API/Apps/Request/consumers.py
@@ -12,12 +12,12 @@ class RequestConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.nickname = self.scope["url_route"]["kwargs"]["nickname"]
         self.group_name = "requests"
+        self.channel_name = f"nickname_{self.nickname}"  # Set the channel name here
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
         await self.accept()
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.group_name,
@@ -27,17 +27,28 @@ class RequestConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         await self.create_request(text_data_json)
-        await self.channel_layer.group_send(
-            self.group_name,
+        receiver_nickname = text_data_json["receiver"]
+        receiver_channel_name = f"nickname_{receiver_nickname}"
+        await self.channel_layer.send(
+            receiver_channel_name,
             {
+                "type": "create_request_message",
                 "sender": text_data_json["sender"],
-                "receiver": text_data_json["receiver"],
+                "receiver": receiver_nickname,
                 "request_type": text_data_json["request_type"]
             }
         )
 
+    async def create_request_message(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': event['type'],
+            'sender': event['sender'],
+            'receiver': event['receiver'],
+            'request_type': event['request_type'],
+        }))
+
     async def create_request(self, event):
-        print(event)
         sender = event["sender"]
         receiver = event["receiver"]
         request_type = event["request_type"]
--- Backend/static/scripts/requests.js
+++ Backend/static/scripts/requests.js
@@ -3,19 +3,18 @@ import {notify} from "../components/Notification.js";
 async function App()
 {
     const nickname = localStorage.getItem('activeUserNickname');
-      let socket = new WebSocket(`ws://localhost:8000/ws/requests/${nickname}`);
-  socket.onopen = function (e) {
-    console.log("Sending to server");
+    let socket = new WebSocket(`ws://localhost:8000/ws/requests/${nickname}`);
+    socket.onopen = function (e) {
     let data = {
         'sender':nickname,
-        'receiver':'test123',
+        'receiver':nickname === 'MKM' ? 'test123' : 'MKM',
         'request_type':"friend"
     }
     socket.send(JSON.stringify(data));
   }
   socket.onmessage = function (e) {
+      console.log("Received from server");
       notify("New request", 3, "success")
   }
 }
-
 App().catch((err) => console.error(err));
