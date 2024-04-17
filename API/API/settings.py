de963d65e96d4b76d7ab7ed87a54c707a91cfe7a
yugurlu <yusufugurlu39@outlook.com>
2024-04-17 22:21:00 +0300

    game start

--- API/API/settings.py
+++ API/API/settings.py
@@ -100,7 +100,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("172.20.0.2", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
