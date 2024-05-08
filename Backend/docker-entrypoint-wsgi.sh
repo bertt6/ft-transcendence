01883918d79b4fed0af82c54f5c7ab4b455283bf
MKM <kaanmesum@gmail.com>
2024-05-08 15:21:17 +0300

    t

--- Backend/docker-entrypoint-wsgi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 10
-
-python manage.py collectstatic --no-input
-
-echo "Starting gunicorn server"
-gunicorn Backend.wsgi:application --bind 0.0.0.0:3030 --preload --timeout 90 -w 4
