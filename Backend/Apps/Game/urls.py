e085e67a4cb4e5ac020cd4b50a0a7afa7891904b
MKM327 <85278762+MKM327@users.noreply.github.com>
2024-05-08 13:15:59 +0300

    Delete Backend directory Gereksiz

--- Backend/Apps/Game/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('<uuid:game_id>/',views.index,name='index'),
-]
--- Backend/Apps/Profile/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request,profile_username=None):
-    return render(request, 'profile/profile.html')
